732656ee094fd22195edb1fc66623fcefea4d7fd
Darren Loasby <darren.loasby@gmail.com>
2018-12-15 22:15:09 +1100

    Update user.rb

--- app/models/identity_tijuana/user.rb
+++ app/models/identity_tijuana/user.rb
@@ -11,15 +11,18 @@ module IdentityTijuana
         firstname: first_name,
         lastname: last_name,
         email: email,
-        address1: street_address, city: suburb, zipcode: postcode.try(:number),
+        address1: street_address,
+        city: suburb,
+        zipcode: postcode.try(:number),
         doNotContact: !is_member
       }
-      c = ISO3166::Country.new(country_iso) || ISO3166::Country.new('AU')
-      member_hash[:country] = c.name if country_iso
-      member_hash[:state] = c.states[postcode.state]["name"] if postcode.try(:state).present?
+      c = ISO3166::Country.new(country_iso || 'AU')
+      member_hash[:country] = c.name if country_iso or postcode
+      member_hash[:state] = c.states[postcode.state]["name"] if postcode
       member_hash[:phone] = home_number if home_number.present?
       member_hash[:mobile] = mobile_number if mobile_number.present?
-
+      member_hash[:mobile] = tags.join(',')
+      
       Lead.delay.upsert_lead(member_hash)
     end
   end
