03394c015f7b1825b6df22fb61a6c11accd10e70
Darren Loasby <darren.loasby@gmail.com>
2018-12-15 22:41:50 +1100

    Update user.rb

--- app/models/identity_tijuana/user.rb
+++ app/models/identity_tijuana/user.rb
@@ -3,7 +3,7 @@ module IdentityTijuana
     include ReadOnly
     self.table_name = 'users'
     has_many :taggings, as: :taggable, source_type: 'User'
-    has_many :tags, through: :taggings
+    has_many :tags, through: :taggings, source_type: 'User'
     belongs_to :postcode, optional: true
 
     def import
