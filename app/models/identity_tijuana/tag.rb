fcecc5ff8a47dceb66edb67e66f03f491ec19e91
Darren Loasby <darren.loasby@gmail.com>
2018-12-15 23:23:41 +1100

    Update tag.rb

--- app/models/identity_tijuana/tag.rb
+++ app/models/identity_tijuana/tag.rb
@@ -3,6 +3,6 @@ module IdentityTijuana
     include ReadWrite
     self.table_name = 'tags'
     has_many :taggings
-    has_many :users, through: :taggings, source_type: User
+    has_many :users, through: :taggings, class_type: User
   end
 end
