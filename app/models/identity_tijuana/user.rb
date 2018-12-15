9ae869ef6149e79d1d1d4d27344b9f476741e6f2
Darren Loasby <darren.loasby@gmail.com>
2018-12-15 15:48:19 +1100

    Update user.rb

--- app/models/identity_tijuana/user.rb
+++ app/models/identity_tijuana/user.rb
@@ -1,6 +1,6 @@
 module IdentityTijuana
   class User < ApplicationRecord
-    include ReadWrite
+    include ReadOnly
     self.table_name = 'users'
     has_many :taggings
     has_many :tags, through: :users
@@ -8,26 +8,18 @@ module IdentityTijuana
 
     def import
       member_hash = {
-        ignore_phone_number_match: true,
         firstname: first_name,
         lastname: last_name,
-        emails: [{ email: email }],
-        phones: [],
-        addresses: [{ line1: street_address, town: suburb, country: country_iso, state: postcode.try(:state), postcode: postcode.try(:number) }],
-        external_ids: { tijuana: id },
-        subscriptions: [{ id: Subscription::EMAIL_SUBSCRIPTION, action: is_member ? 'subscribe' : 'unsubscribe' }]
+        email: email,
+        address1: street_address, city: suburb, country: country_iso, state: postcode.try(:state), zipcode: postcode.try(:number),
+        doNotContact: is_member
       }
-      if Settings.tijuana.opt_out_subscription_id
-        member_hash[:subscriptions].push({
-          id: Settings.tijuana.opt_out_subscription_id,
-          action: do_not_call ? 'unsubscribe' : 'subscribe'
-        })
-      end
+      
 
-      member_hash[:phones].push(phone: home_number) if home_number.present?
-      member_hash[:phones].push(phone: mobile_number) if mobile_number.present?
+      member_hash[:phone] = home_number if home_number.present?
+      member_hash[:mobile] = mobile_number if mobile_number.present?
 
-      Member.delay.upsert_member(member_hash, 'tijuana:fetch_updated_users')
+      Lead.delay.upsert_lead(member_hash, 'tijuana:fetch_updated_users')
     end
   end
 end
