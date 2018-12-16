d1e076606960f315f90983d2201964fe9b498cbd
Darren Loasby <darren.loasby@gmail.com>
2018-12-16 11:47:31 +1100

    Update tagging.rb

--- app/models/identity_tijuana/tagging.rb
+++ app/models/identity_tijuana/tagging.rb
@@ -2,7 +2,7 @@ module IdentityTijuana
   class Tagging < ApplicationRecord
     include ReadWrite
     self.table_name = 'taggings'
-    belongs_to :tag
-    belongs_to :taggable, polymorphic: true
+#     belongs_to :tag
+#     belongs_to :taggable, polymorphic: true
   end
 end
