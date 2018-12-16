e0cc2c23cb862442304d498159c0200106ff47ed
Darren Loasby <darren.loasby@gmail.com>
2018-12-16 11:26:09 +1100

    Update postcode.rb

--- app/models/identity_tijuana/postcode.rb
+++ app/models/identity_tijuana/postcode.rb
@@ -1,5 +1,8 @@
 module IdentityTijuana
   class Postcode < ApplicationRecord
+    def self.name
+      "Postcode"
+    end
     include ReadWrite
     self.table_name = 'postcodes'
     has_many :users
